59deafca31ed440e79edd7ed81f41c4d75d08e7c
Tod E. Kurt <tod@todbot.com>
2018-03-19 01:20:56 -0700

    some cleanup

--- efm32hg-neopixels/main.c
+++ efm32hg-neopixels/main.c
@@ -1,34 +1,11 @@
 /*****************************************************************************
- * @file spi_master.c
- * @brief DMA SPI master transmit/receive example
- * @author Silicon Labs
- * @version 2.06
- ******************************************************************************
- * @section License
- * <b>(C) Copyright 2014 Silicon Labs, http://www.silabs.com</b>
- *******************************************************************************
  *
- * Permission is granted to anyone to use this software for any purpose,
- * including commercial applications, and to alter it and redistribute it
- * freely, subject to the following restrictions:
+ * EFM32HG Neopixels using SPI
  *
- * 1. The origin of this software must not be misrepresented; you must not
- *    claim that you wrote the original software.
- * 2. Altered source versions must be plainly marked as such, and must not be
- *    misrepresented as being the original software.
- * 3. This notice may not be removed or altered from any source distribution.
- *
- * DISCLAIMER OF WARRANTY/LIMITATION OF REMEDIES: Silicon Labs has no
- * obligation to support this Software. Silicon Labs is providing the
- * Software "AS IS", with no express or implied warranties of any kind,
- * including, but not limited to, any implied warranties of merchantability
- * or fitness for any particular purpose or warranties against infringement
- * of any proprietary rights of a third party.
- *
- * Silicon Labs will not be liable for any consequential, incidental, or
- * special damages, or any other relief, or for any claim by any third party,
- * arising from your use of this Software.
- *
+ * 2018, Tod E. Kurt, http://todbot.com/blog
+ * 
+ * Technique from https://jeelabs.org/book/1450d/
+ * 
  ******************************************************************************/
 
 #include <stdbool.h>
@@ -50,19 +27,8 @@
 DMA_CB_TypeDef spiCallback;
 
 /* Transfer Flags */
-volatile bool rxActive;
 volatile bool txActive;
 
-/* SPI Data Buffers */
-
-const uint16_t spiTxData2[] = {
-                                0b000000000010,
-                                0b000000000101,
-                                0b010000000000, 
-                                0b011000000000,
-                                0x0123,
-                                0x0456, 0x0DEF, 0x0ABA, 0x0CAB, 0x0B0B };
-int spiTxData2Len = (sizeof(spiTxData2)/sizeof(uint16_t));
 
 
 // The uptime in milliseconds, maintained by the SysTick timer.
@@ -78,16 +44,15 @@ void SysTick_Handler() {
 void SpinDelay(uint32_t millis) {
   // Calculate the time at which we need to finish "sleeping".
   uint32_t sleep_until = uptime_millis + millis;
-
+  
   // Spin until the requested time has passed.
   while (uptime_millis < sleep_until);
 }
 void setupSysTick() {
-    // Sets up and enable the `SysTick_Handler' interrupt to fire once every 1ms.
+  // Sets up and enable the `SysTick_Handler' interrupt to fire once every 1ms.
   // ref: http://community.silabs.com/t5/Official-Blog-of-Silicon-Labs/Chapter-5-MCU-Clocking-Part-2-The-SysTick-Interrupt/ba-p/145297
   if (SysTick_Config(CMU_ClockFreqGet(cmuClock_CORE) / 1000)) {
-    // Something went wrong.
-    while (1);
+    while (1);      // Something went wrong.
   }
 }
 
@@ -123,43 +88,25 @@ void setupSpi(void)
 {
   USART_InitSync_TypeDef usartInit = USART_INITSYNC_DEFAULT;  
   
-  /* Initialize SPI */
-  //usartInit.databits = usartDatabits8;
+  // Initialize SPI 
   usartInit.databits = usartDatabits12;
-  //usartInit.baudrate = 1000000;
   usartInit.baudrate = 2400000;
   usartInit.msbf = true;
-  //usartInit.baudrate = 12000000;
-  //usartInit.baudrate = 20000000;
+
   USART_InitSync(USART0, &usartInit);
   
-  /* Turn on automatic Chip Select control */
-  //USART0->CTRL |= USART_CTRL_AUTOCS;
-  
-  /* Enable SPI transmit and receive */
+  // Enable SPI transmit and receive 
   USART_Enable(USART0, usartEnable);
   
   // Configure GPIO pins for SPI 
   GPIO_PinModeSet(gpioPortE, 12, gpioModePushPull, 0); // CLK
   GPIO_PinModeSet(gpioPortE, 10, gpioModePushPull, 0); // MOSI 
-  //GPIO_PinModeSet(gpioPortD, 1, gpioModeInput,    0); // MISO
-  //GPIO_PinModeSet(gpioPortD, 3, gpioModePushPull, 1); // CS 
  
-  // Route USART clock to display clock and USART TX to display SI
+  // Route USART clock and USART TX to LOC0 (PortE12, PortE10)
   USART0->ROUTE = USART_ROUTE_LOCATION_LOC0 |
                   USART_ROUTE_CLKPEN |
                   USART_ROUTE_TXPEN;
-  
-  /*
-  // Enable routing for SPI pins from USART to location 1 
-  USART0->ROUTE = USART_ROUTE_TXPEN | 
-                  USART_ROUTE_RXPEN | 
-                  USART_ROUTE_CSPEN | 
-                  USART_ROUTE_CLKPEN | 
-                  USART_ROUTE_LOCATION_LOC1;
-  */
 }
-
 
 
 /**************************************************************************//**
@@ -167,7 +114,7 @@ void setupSpi(void)
  *****************************************************************************/
 void setupDma(void)
 {
-  /* Initialization structs */
+  /* Initialization structs */ 
   DMA_Init_TypeDef        dmaInit;
   DMA_CfgChannel_TypeDef  txChnlCfg;
   DMA_CfgDescr_TypeDef    txDescrCfg;
@@ -200,8 +147,6 @@ void setupDma(void)
   DMA_CfgDescr(DMA_CHANNEL_TX, true, &txDescrCfg);
 }
 
-//
-//void spiDmaTransferOut(uint8_t *txBuffer, int bytes)
 void spiDmaTransferOut2(uint8_t *txBuffer, int count)
 {
   // Setting flag to indicate that TX is in progress
@@ -221,19 +166,15 @@ void spiDmaTransferOut2(uint8_t *txBuffer, int count)
 }
 
 
-
-
 /**************************************************************************//**
  * @brief  Returns if an SPI transfer is active
  *****************************************************************************/
 bool spiDmaIsActive(void)
 {
   bool temp;
-  temp = rxActive;
-  temp = temp | txActive;
+  temp = txActive;
   return temp;
 }
-
 
 
 /**************************************************************************//**
@@ -274,6 +215,10 @@ typedef struct {
   uint8_t g;
   uint8_t b;
 } rgb_t;
+
+
+// this idea from JeeLabs:
+// https://jeelabs.org/book/1450d/
 
 static const uint16_t bits[] = {
     0b100100100100,
@@ -321,11 +266,6 @@ static void sendLEDs(rgb_t* leds, int num)
 
 /**************************************************************************//**
  * @brief  Main function
- * This example sets up the DMA to transfer outbound and incoming data from the
- * SPI (USART0) to/from the source/destination buffers. Three tests are done:
- * 1) Transmit data (string) without reading received data
- * 2) Transmit data (string) and transfer received data to RAM buffer
- * 3) Transmit dummy data and transfer received data to RAM buffer
  *****************************************************************************/
 int main(void)
 { 
@@ -335,40 +275,21 @@ int main(void)
   // Disable the watchdog that the bootloader started.
   WDOG->CTRL = 0;
   
+  // Configuring clocks in the Clock Management Unit (CMU) 
+  CMU_ClockEnable(cmuClock_DMA, true);  
+  CMU_ClockEnable(cmuClock_USART0, true);  
   // Switch on the clock for GPIO. Even though there's no immediately obvious
   // timing stuff going on beyond the SysTick below, it still needs to be
   // enabled for the GPIO to work.
   CMU_ClockEnable(cmuClock_GPIO, true);
-  GPIO_PinModeSet(gpioPortF, 4, gpioModePushPull, 0);
-  GPIO_PinModeSet(gpioPortF, 5, gpioModePushPull, 0);
 
-  // Configuring clocks in the Clock Management Unit (CMU) 
-  setupCmu();
-  
   // Configura USART for SPI 
   setupSpi();
 
   // configure SysTick handler for millis 
   setupSysTick();
 
-  
-  GPIO_PinOutSet(gpioPortF, 4);
-  /*
-  while( 1 ) { 
-    GPIO_PinOutSet(gpioPortF, 4);
-    SpinDelay(100);
-    GPIO_PinOutClear(gpioPortF, 4);
-    SpinDelay(100);
-  }
-  */
-
-  /*
-  while(1) {
-    for( int i=0x100; i<0x350; i++) {
-      USART_TxDouble( USART0, i);
-    }
-  }
-  */
+  sendRGB( 0,0,0 ); // just to test
 
   rgb_t leds[8]=
     {
@@ -394,46 +315,33 @@ int main(void)
     SpinDelay(100);
   }
   
-  while( 1 ) {
-    
-    sendRGB( 0x40, 0x00, 0x00 );
-    sendRGB( 0x00, 0x40, 0x00 );
-    sendRGB( 0x00, 0x00, 0x40 );
-    sendRGB( 0x40, 0x00, 0x40 );
-    sendRGB( 0x40, 0x40, 0x00 );
-    sendRGB( 0x00, 0x40, 0x40 );
-    sendRGB( 0x40, 0x40, 0x40 );
-    sendRGB( 0x10, 0x10, 0x10 );
-    SpinDelay(500);
-    
-    sendRGB( 0x00, 0x00, 0x00 );
-    sendRGB( 0x00, 0x00, 0x00 );
-    sendRGB( 0x00, 0x00, 0x00 );
-    sendRGB( 0x00, 0x00, 0x00 );
-    sendRGB( 0x00, 0x00, 0x00 );
-    sendRGB( 0x00, 0x00, 0x00 );
-    sendRGB( 0x00, 0x00, 0x00 );
-    sendRGB( 0x00, 0x00, 0x00 );
-    SpinDelay(500); 
- }
   
-  /* Configure DMA transfer from RAM to SPI using ping-pong */      
+  // more tests
+  const uint16_t spiTxData2[] = {
+                                0b000000000010,
+                                0b000000000101,
+                                0b010000000000, 
+                                0b011000000000,
+                                0x0123,
+                                0x0456, 0x0DEF, 0x0ABA, 0x0CAB, 0x0B0B };
+  int spiTxData2Len = (sizeof(spiTxData2)/sizeof(uint16_t));
+
+
+  // Configure DMA transfer from RAM to SPI using ping-pong
   setupDma();
 
   while( 1 ) {
     // Send data out
-    spiDmaTransferOut2((uint8_t*) spiTxData2, spiTxData2Len);
+    //spiDmaTransferOut2((uint8_t*) spiTxData2, spiTxData2Len);
   
-    GPIO_PinOutSet(gpioPortF, 5);
-    
     // Sleep until DMA is done 
     sleepUntilDmaDone();
   }
   
  
-  /* Cleaning up after DMA transfers */
+  // Cleaning up after DMA transfers 
   DMA_Reset();
 
-  /* Done */
+  // Done 
   while (1);
 }
